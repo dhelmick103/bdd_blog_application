34c2126d0a25a35de0ed46a817b3896c2efad1c9
dhelmick103 <dhelmick103@gmail.com>
2016-12-26 20:57:46 +0000

    Handle article not found error

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "Showing an article" do
   end 
   
   scenario "A user shows an article" do 
-    vist "/"
+    visit "/"
     click_link @article.title 
     
     expect(page).to have_content(@article.title)
