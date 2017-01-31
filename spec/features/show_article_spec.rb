1bb3df76ca5d201a3021016e8c30a195c92e5a48
dhelmick103 <dhelmick103@gmail.com>
2017-01-31 11:52:25 +0000

    Fix broken show article spec

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -1,18 +1,20 @@
 require "rails_helper"
 
-RSpec.feature "Showing an article" do 
+RSpec.feature "Showing an article" do
   
-  before do 
-    @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.")
-  end 
+  before do
+    john = User.create(email: "john@example.com", password: "password")
+    login_as(john)
+    @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.", user: john)
+  end
   
-  scenario "A user shows an article" do 
+  scenario "A user shows an article" do
     visit "/"
-    click_link @article.title 
+    click_link @article.title
     
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
-    expect(current_path).to eq (article_path(@article))
-  end 
+    expect(current_path).to eq(article_path(@article))
+  end
   
-end 
+end
